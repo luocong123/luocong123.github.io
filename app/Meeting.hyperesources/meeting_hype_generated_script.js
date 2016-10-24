f68266764a6269be18dca707f935ebcd421188cd
罗聪 <ben.luo@RW-IT-BEN.local>
2016-10-24 11:07:47 +0800

    Site updated: 2016-10-24 11:07:47

--- app/Meeting.hyperesources/meeting_hype_generated_script.js
+++
@@ -1,5 +0,0 @@
-//	HYPE.documents["Meeting"]
-
-(function(){(function k(){function l(a,b,d){var c=!1;null==window[a]&&(null==window[b]?(window[b]=[],window[b].push(k),a=document.getElementsByTagName("head")[0],b=document.createElement("script"),c=h,false==!0&&(c=""),b.type="text/javascript",b.src=c+"/"+d,a.appendChild(b)):window[b].push(k),c=!0);return c}var h="Meeting.hyperesources",c="Meeting",e="meeting_hype_container";if(false==!1)try{for(var f=document.getElementsByTagName("script"),
-a=0;a<f.length;a++){var b="f[a].src;if(null!=b&&-1!=b.indexOf("meeting_hype_generated_script.js")){h=b.substr(0,b.lastIndexOf("/"));break}}}catch(n){}if(false==!1&&(a=navigator.userAgent.match(/MSIE" (\d+\.\d+)="" ),a="parseFloat(a&&a[1])||null,a=l("HYPE_530","HYPE_dtl_530",!0==(null!=a&&10">a||false==!0)?"HYPE-530.full.min.js":"HYPE-530.thin.min.js"),false==!0&&(a=a||l("HYPE_w_530","HYPE_wdtl_530","HYPE-530.waypoints.min.js")),a))return;
-f=window.HYPE.documents;if(null!=f[c]){b=1;a=c;do c=""+a+"-"+b++;while(null!=f[c]);for(var d=document.getElementsByTagName("div"),b=!1,a=0;a</f.length;a++){var>
